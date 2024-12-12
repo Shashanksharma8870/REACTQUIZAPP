9696b9db0839eeac8dd6e082cdb78c4569ea405e
Shashank sharma <146346404+Shashanksharma8870@users.noreply.github.com>
2024-12-12 13:30:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+      question: "What is the capital of France?",
+      opption1:"Berlin",
+      opption2:"Madrid",
+      opption3:"Paris",
+      opption4:"Rome",
+      ans: 3,
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      opption1:"Earth", 
+      opption2:"Mars", 
+      opption3:"Jupiter",
+      opption4:"Venus",
+      ans: 2,
+    },
+    {
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      opption1:"Harper Lee",  
+      opption2:"Mark Twain",
+      opption3:"Ernest Hemingway",
+      opption4:"F. Scott Fitzgerald",
+      ans: 1,
+    },
+    {
+      question: "What is the chemical symbol for water?",
+      opption1:"H2O",
+      opption2:"O2",
+      opption3:"CO2",
+      opption4:"HO2",
+      ans: 1,
+    },
+    {
+      question: "Which year did the Titanic sink?",
+      opption1:"1912",
+      opption2:"1905",
+      opption3:"1898",
+      opption4:"1920",
+      ans: 1,
+    }
+  ];
+  
